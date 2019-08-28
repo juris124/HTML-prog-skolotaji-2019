8c2f67340881d935e9c3276c0e586e9dda75b74d
juris124 <31391698+juris124@users.noreply.github.com>
2019-08-28 14:03:34 +0300

    Apakšmape ar index

--- hipotenuza.js
+++ hipotenuza.js
@@ -1,4 +1,4 @@
-// hipotenuzas funkcijas
+// hipotenuzas funkcija
 function hipo(x,y){
     return Math.sqrt(x*x+y*y);
 }
--- masivs.js
+++ masivs.js
@@ -1,21 +1,32 @@
-let masivs=[];
+let masivs = [];
+
+function aizpildaMasivu(v){
+    for(let j=0;j<v;j++){
+        masivs[j]=j+1;
+    }
+}
 
 function iegutPecIndex(i) {
-    return masivs[i];
+  return masivs[i];
 }
 
-function mainitPecIndex(i, vertiba){
-    masivs[i]=vertiba;
+function mainitPecIndex(i, vertiba) {
+  masivs[i] = vertiba;
 }
 
-function dzestPecIndex(i,veriba){
-    masivs[i]=masivs.splice(i,1)
+function dzestPecIndex(i, veriba) {
+  masivs[i] = masivs.splice(i, 1);
 }
 
-function atrastVertibasIndexu(vertiba){
-    for (let i=0;i<masivs.length;i++){ // var arii ar do .. while ..
-        if (vertiba===masivs[i]){
-            return 1;
-        }
+function printMasivs(){
+    console.log(masivs);
+}
+
+function atrastVertibasIndexu(vertiba) {
+  for (let i = 0; i < masivs.length; i++) {
+    // var arii ar do .. while .., arī while ..
+    if (vertiba === masivs[i]) {
+      return 1;
     }
+  }
 }
